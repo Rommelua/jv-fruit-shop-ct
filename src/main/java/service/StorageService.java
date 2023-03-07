2fbde24d7a095a72050c20cbfe19e7c56851ecc2
Serhii Zaitsev <zaytsevsergey2013@gmail.com>
2023-03-07 14:58:34 +0200

    updated name for a method in TransactionParser and TransactionParserImpl

--- src/main/java/service/StorageService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import db.Storage;
-import java.util.List;
-import model.FruitTransaction;
-
-public interface StorageService {
-    Storage createStorage(List<FruitTransaction> transactionList);
-}
